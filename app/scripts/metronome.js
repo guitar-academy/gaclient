c8ae42eb7747646763085c7ecb9c5987a57fd269
Diga Widyaprana <digawp@gmail.com>
2014-12-08 21:56:14 +0800

    Make note resolution modifiable

--- app/scripts/metronome.js
+++ app/scripts/metronome.js
@@ -81,7 +81,10 @@ Metronome.prototype.play = function() {
     }
 }
 
-Metronome.prototype.init = function(){
+Metronome.prototype.init = function(noteRes){
+    if (noteRes != null) {
+        this.noteResolution = noteRes;
+    }
     // NOTE: THIS RELIES ON THE MONKEYPATCH LIBRARY BEING LOADED FROM
     // Http://cwilso.github.io/AudioContext-MonkeyPatch/AudioContextMonkeyPatch.js
     // TO WORK ON CURRENT CHROME!!  But this means our code can be properly
